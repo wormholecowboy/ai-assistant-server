1395e59b313b0cc50e263484a48bc49d926565f7
wormhole cowboy <wormholecowboy@gmail.com>
2025-02-19 21:44:37 -0500

    Added server and gemini api

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
 from pydantic import BaseModel
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from google import genai
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+gemini_client = genai.Client(api_key=GEMINI_API_KEY)
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    contents = "This is just an api test. Say hello in any language you like!"
+    response = gemini_client.models.generate_content(
+        model="gemini-2.0-flash", contents=contents
+    )
+    print(response.text)
+
+    return {"message": response.text}
